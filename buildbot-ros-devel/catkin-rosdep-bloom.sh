b43fbf3e41725f4268e3e36a1bd2d1a398efd351
Chengming Zhou <eric.zcm@cainiao.com>
2018-03-12 18:56:08 -0700

    env.sh: move cowbuilder-update outside and check reinit failure

--- buildbot-ros-devel/catkin-rosdep-bloom.sh
+++ buildbot-ros-devel/catkin-rosdep-bloom.sh
@@ -19,7 +19,9 @@ port=4500
 distro=xenial
 component=main
 
+if ! [ -f /etc/ros/rosdep/sources.list.d/20-default.list ]; then
 sudo rosdep init || exit 1
+fi
 sudo rm /etc/ros/rosdep/sources.list.d/20-default.list
 echo "yaml http://${url}:${port}/private.yaml" | sudo tee /etc/ros/rosdep/sources.list.d/10-private.list > /dev/null
 # and replace official, it's too slow
@@ -37,12 +39,12 @@ echo | sudo tee /etc/ros/rosdep/sources.list.d/30-local.list > /dev/null
 rosdep update || exit 1
 
 # private ros pkgs
-echo "deb http://${url}:$port $distro $component" | sudo tee /etc/apt/sources.list.d/ros-private.list > /dev/null
 wget -O - -q http://${url}:${port}/apt.key | sudo apt-key add -
-sudo -E apt-get update -o Dir::Etc::sourcelist=/etc/apt/sources.list.d/ros-private.list || exit 1
+echo "deb http://${url}:$port $distro $component" | sudo tee /etc/apt/sources.list.d/ros-private.list > /dev/null
+sudo -E apt-get update -o Dir::Etc::sourcelist=/etc/apt/sources.list.d/ros-private.list
 
 # official ros pkgs
-sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
-sudo apt-get update || exit 1
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+sudo apt-get update
 
--- buildbot-ros-devel/cowbuilder-setup.sh
+++ buildbot-ros-devel/cowbuilder-setup.sh
@@ -8,6 +8,7 @@ sudo apt-get install -y dpkg-dev debhelper || exit 1
 # + local apt repo in /var/cache/pbuilder/result
 # the hook will scan all debs in there and apt-get update
 sudo cp hooks/D70results /var/cache/pbuilder/hooks
+rm ~/.pbuilderrc
 echo 'AUTO_DEBSIGN=${AUTO_DEBSIGN:-no}' >> ~/.pbuilderrc
 echo 'HOOKDIR=/var/cache/pbuilder/hooks' >> ~/.pbuilderrc
 echo 'BINDMOUNTS="/var/cache/pbuilder/result"' >> ~/.pbuilderrc
@@ -16,5 +17,5 @@ sudo chmod 777 /var/cache/pbuilder/result
 # Need sudo without password: auto, You should set sudoers at the begining
 # visudo:
 # buildbot	ALL= NOPASSWD: SETENV: /usr/bin/gbp, /usr/bin/git-*, /usr/sbin/*builder
-cowbuilder-update.py xenial amd64 || exit 1
+./cowbuilder-update.py xenial amd64 || exit 1
 
